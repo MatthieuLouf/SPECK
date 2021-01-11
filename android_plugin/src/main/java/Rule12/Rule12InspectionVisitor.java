03da88a51c439c7bab23a6f30709874e5e048657
Matthieu Louf <44584413+MatthieuLouf@users.noreply.github.com>
2021-01-11 11:14:28 +0100

    implement first Rule12 Inspection

--- android_plugin/src/main/java/Rule12/Rule12InspectionVisitor.java
+++ android_plugin/src/main/java/Rule12/Rule12InspectionVisitor.java
@@ -1,27 +1,42 @@
 package Rule12;
 
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.psi.JavaElementVisitor;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class Rule12InspectionVisitor extends JavaElementVisitor {
 
     ProblemsHolder problemsHolder;
+
+    final String methodToDetect = "getSharedPreferences";
+    final String methodArgsToDetect = "MODE_PRIVATE";
+
+    final String errorMessage = "Shared preference(s) saving (are) not private";
 
     public Rule12InspectionVisitor(@NotNull ProblemsHolder holder) {
         problemsHolder = holder;
     }
 
     @Override
-    public void visitJavaFile(PsiJavaFile file) {
-        super.visitJavaFile(file);
-    }
-
-    @Override
-    public void visitElement(@NotNull PsiElement element) {
-        super.visitElement(element);
+    public void visitMethodCallExpression(PsiMethodCallExpression expression) {
+        if(Objects.equals(expression.getMethodExpression().getReferenceName(), methodToDetect))
+        {
+            PsiExpression[] psiExpressions = expression.getArgumentList().getExpressions();
+            boolean argToDetectIsPresent = false;
+            for (PsiExpression psiExpression: psiExpressions) {
+                if (psiExpression.getText().contains(methodArgsToDetect) || psiExpression.getText().contains("0")) {
+                    argToDetectIsPresent = true;
+                    break;
+                }
+            }
+            if(!argToDetectIsPresent)
+            {
+                problemsHolder.registerProblem(expression, errorMessage);
+            }
+        }
+        super.visitMethodCallExpression(expression);
     }
 
 }
--- android_plugin/src/main/java/Rule8/Rule8InspectionVisitor.java
+++ android_plugin/src/main/java/Rule8/Rule8InspectionVisitor.java
@@ -4,6 +4,7 @@ import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Rule8InspectionVisitor extends JavaElementVisitor {
@@ -13,30 +14,28 @@ public class Rule8InspectionVisitor extends JavaElementVisitor {
     final String methodToDetect = "openFileOutput";
     final String methodArgsToDetect = "MODE_PRIVATE";
 
+    final String errorMessage = "Call(s) to internal storage (are) not private";
+
     public Rule8InspectionVisitor(@NotNull ProblemsHolder holder) {
         problemsHolder = holder;
     }
 
     @Override
     public void visitMethodCallExpression(PsiMethodCallExpression expression) {
-        if(Objects.equals(expression.getMethodExpression().getReferenceName(), methodToDetect))
-        {
+        if (Objects.equals(expression.getMethodExpression().getReferenceName(), methodToDetect)) {
             PsiExpression[] psiExpressions = expression.getArgumentList().getExpressions();
+
             boolean argToDetectIsPresent = false;
-            for (PsiExpression psiExpression: psiExpressions) {
+            for (PsiExpression psiExpression : psiExpressions) {
                 if (psiExpression.getText().contains(methodArgsToDetect) || psiExpression.getText().contains("0")) {
                     argToDetectIsPresent = true;
                     break;
                 }
             }
-            if(!argToDetectIsPresent)
-            {
-                problemsHolder.registerProblem(expression, "Call(s) to internal storage (are) not private");
+            if (!argToDetectIsPresent) {
+                problemsHolder.registerProblem(expression, errorMessage);
             }
         }
         super.visitMethodCallExpression(expression);
-
-        //
-
     }
 }
